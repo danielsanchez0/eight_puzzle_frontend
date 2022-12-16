600f65d714311738ed1eedb7244687e566dcfeef
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2022-12-15 20:24:54 -0500

    quitar lista

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -28,31 +28,6 @@ function Menu(args) {
         }}
       >
         <NavbarBrand href="/">Sistemas Inteligentes I</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="me-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">
-                GitHub
-              </NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>Option 1</DropdownItem>
-                <DropdownItem>Option 2</DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>Reset</DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-          <NavbarText>Simple Text</NavbarText>
-        </Collapse>
       </Navbar>
     </div>
   );
